980339171f9b87944475b841b931f2696b6318e1
Tulio Hernandez <tehp1993@gmail.com>
2014-03-20 15:42:35 -0500

    se ha cambiado el el index customers para que se muestre en jqGrid y muestre la vista con un doble click

--- app/Controller/CustomersController.php
+++ app/Controller/CustomersController.php
@@ -4,8 +4,8 @@ class CustomersController extends AppController {
    
     
 
-    public $helpers = array('Html','Form');
-    public $components = array('Session', 'Paginator');
+    public $helpers = array('Html','Form', 'Js');
+    public $components = array('Session', 'Paginator', 'RequestHandler');
     public $name = 'Customers';
    
 
@@ -36,10 +36,7 @@ class CustomersController extends AppController {
 
 
        public function index() {
-        $this->Customer->recursive = 0;
-        $this->set('Customers', $this->paginate());
-        $this->set('Customers', $this->Customer->find('all'));
-
+		
         //municipalty id = 2 corresponde al id del departamento de antioquia
 
         $this->set('total_customer', $total_customer = $this->Customer->find('count', array('conditions' => array('Customer.state !=' => 'Inactivo'))));
@@ -205,7 +202,88 @@ class CustomersController extends AppController {
     return parent::isAuthorized($user);
     }
 
+function showGrid()
+        {
 
+    // get how many rows we want to have into the grid - rowNum parameter in the grid
+   
+    $limit = $this->request->query['rows'];
+
+    // get index row - i.e. user click to sort. At first time sortname parameter -
+    // after that the index from colModel
+    $sidx = $this->request->query['sidx'];
+
+    // sorting order - at first time sortorder
+    $sord = $this->request->query['sord'];
+
+    $page = $this->request->query['page'];
+
+
+    // if we not pass at first time index use the first column for the index or what you want
+    if( !$sidx ) $sidx = 1;
+            //calculate no of rows from query
+            $row=$this->Customer->find('count');
+            $count = $row;
+
+    // calculate the total pages for the query
+    if( $count > 0 )
+    {
+        $total_pages = ceil($count / $limit);
+    }
+    else
+    {
+        $total_pages = 0;
+    }
+
+    // if for some reasons the requested page is greater than the total
+    // set the requested page to total page
+    if( $page > $total_pages ) $page = $total_pages;
+
+    // calculate the starting position of the rows
+    $start = $limit * $page - $limit;
+
+    // if for some reasons start position is negative set it to 0
+    // typical case is that the user type 0 for the requested page
+    if( $start < 0 ) $start = 0;
+	
+    //fetch only pure data avoiding unnecessay loading of related/associated data
+            $this->Customer->recursive=-1;
+	if(!empty($this->request->query['filters'])){
+		$rule=$this->request->query['filters']['rule'];
+		$resultado=$this->Customer->find('all',array('fields' => array('id','name','phone','dress','state','created'),'order by' => $sidx.' '.$sord, 'limit' => $start,$limit, 'conditions' => array($rule['field'] => $rule['data'].'%')));
+	}
+else{
+	$resultado=$this->Customer->find('all',array('fields' => array('id','name','phone','dress','state','created'),'order by' => $sidx.' '.$sord, 'limit' => $start,$limit));
+}
+
+
+            
+          // $resultado=$this->Customer->find('all',array('fields' => array('id','name','phone','dress','state','created'),'order' => $sort_range,'limit' => $limit_range)); 
+
+            //setting the response object
+           // print_r ($resultado);
+           // exit();
+            
+            $responce->page=$page;
+            $responce->total_pages=$total_pages;
+            $responce->records=$count;
+            //$res=array('rows' => array(''=>array('id','cell')));
+			$i=0;
+			            
+            foreach($resultado as $row)
+            {
+            	
+                $responce->rows[$i]['id']=$row['Customer']['id'];
+                $responce->rows[$i]['cell']=array($row['Customer']['id'],$row['Customer']['name'],$row['Customer']['phone'],$row['Customer']['dress'],$row['Customer']['state'],$row['Customer']['created']);
+                
+                $i++;
+            }
+
+           echo json_encode($responce);
+
+            exit();
+
+        }
 
 
 
--- app/View/Customers/index.ctp
+++ app/View/Customers/index.ctp
@@ -1,9 +1,19 @@
+<?php 
+  echo $this->Html->css('ui.jqgrid'); ?>
+<?php  echo $this->Html->css('ui.multiselect');?>
+<?php   echo $this->Html->css('jquery-ui-1.9.2.custom');?>
+
+<?php echo $this->Html->script('jqGrid/jquery-1.7.2.min');?>
+<?php echo $this->Html->script('jqGrid/grid.locale-en');?>
+<?php echo $this->Html->script('jqGrid/jquery.jqGrid.min');?>
+<?php echo $this->Html->script('jquery-ui-1.9.2.custom.min');?>
+
 <ol class="breadcrumb">
   <li><?php echo $this->Html->link("Home", array('controller' => 'pages',  'action' => 'index')); ?></li>
   <li class="active">Administración de Clientes</li>
 </ol>
 
-<h3 class "info">Administración de Clientes </h3>
+<h3 class="info">Administración de Clientes </h3>
 
 <div class="container well">
 
@@ -38,58 +48,50 @@ echo $this->Html->link("Nuevo Cliente", array(
 		);
 ?>
 </p>
-</br>
+
+
+<div id="table">
+<table id="list"></table>
+<div id="page"></div>
+  </div>
 
 
 
-<div class="table-responsive">
-  <table class="table table-bordered">
 
-		<tr >
-		<th class="headerlist" align="center" width = "40%" >Cliente</th>
-		<th class="headerlist" align="center" width = "20%" >Telefono</th>
-		<th class="headerlist" align="center" width = "10%" >Dirección</th>
-		<th class="headerlist" align="center" width = "10%" >Estado</th>
-		<th class="headerlist" align="center" width = "10%" >Fecha de Creación</th>
-		<td class="headerlist" width = "10%"colspan ="2" align ="center">Accion</td>
-		
-		</tr>
-		
-		<?php foreach ($Customers as $customer): ?>
-		
-		<tr>
-		<td><?php echo $this->Html->link($customer['Customer']['name'], array('action' => 'view',$customer['Customer']['id']));?></td>
-		<td><?php echo $customer['Customer']['phone']; ?></td>		
-		<td><?php echo $customer['Customer']['dress']; ?></td>
-		<td><?php echo $customer['Customer']['state']; ?></td>
-		<td><?php echo $customer['Customer']['created']; ?></td>
-		<td align="center">
-		<?php 
+<script type="text/javascript">
+jQuery(document).ready(function(){
+	jQuery("#list").jqGrid({
+   	url:'<?php echo $this->Html->url(array("controller" => "Customers", "action" => "showGrid")); ?>',
+	datatype: "json",
+	mtype: "GET",
+   	colNames:['id','Cliente','Telefono','Direccion','Estado','Fecha de Creacion'],
+   	colModel:[
+		{name:'id',index:'id',width:15},
+   		{name:'name',index:'name'},
+   		{name:'phone',index:'phone'},
+   		{name:'dress',index:'dress'},
+   		{name:'state',index:'state',width:50},
+   		{name:'created',index:'created'}
+   	],
+   	rowNum:10,
+	rowList : [5,10,15],
+	
+	rownumWidth: 40,
+   	pager: jQuery('#page'),
+   	sortname: 'id',
+    sortorder: 'asc',
+	caption: "Administracion de Cliente",
+	height:"auto",
+     autowidth: true,
+     viewrecords: true,
+     ondblClickRow: function (rowid) {
+        var rowData = $(this).getRowData(rowid);
+        document.location.href = "customers/view/" + rowData['id'];
+    }
+	});
+    jQuery("#list").navGrid("#page",{del:true,add:false,edit:true,search:false});	
+    jQuery("#list").jqGrid('filterToolbar',{stringResult: true,searchOnEnter : false});
+});
 
-		echo $this->Html->image("icon/edit.png", array(
-		    "tittle" => "Editar",
-
-		    'height'=>'15',
-		    'width'=>'15',
-		    'url' => array('action' => 'edit', $customer['Customer']['id']),
-		));
-
-		?>
-		</td>
-		<td align="center">
-		<?php 
-		echo $this->Form->postLink(
-			  $this->Html->image('icon/trash.png', array('alt' => __('Effacer'), 'height'=>'15',
-		    'width'=>'15')), //le image
-			  array('action' => 'delete',$customer['Customer']['id'] ), //le url
-			  array('escape' => false), //le escape
-			  __('Esta seguro de borrar', $customer['Customer']['id']) //le confirm
-			);
-		?>
-		</td>
-		</tr>
-		<?php endforeach; ?>
-	</table>
-</div>
-
+</script>
 
